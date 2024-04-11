2be62fdb12bec94eff59f3cf0a2e4b7ee7c21402
KheraniAlfaz <alfazkherani@gmail.com>
2024-04-12 02:00:35 +0530

    State changes configured

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.calculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.calculator.ui.theme.CalculatorTheme
+import com.example.calculator.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +23,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             CalculatorTheme {
                 // A surface container using the 'background' color from the theme
-
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    modifier = Modifier
+                        .background(MediumGray)
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    buttonSpacing = buttonSpacing,
+                    onAction = viewModel::onAction)
             }
         }
     }
